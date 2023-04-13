commontk/QtSOAP

commit 81dc56d87b3c8f7659873e7f2238d3e258a3eacb

removed deprecated QString::null
- Build with Qt 5.10
- Removed deprecated QString::null and changed to QString::QString()

src/qtsoap.cpp
@@ -683,11 +683,11 @@ QtSoapType::Type QtSoapType::nameToType(const QString &name)
 }
 
 /*!
-    Returns QString::null.
+    Returns QString::QString().
 */
 QString QtSoapType::toString() const
 {
-    return QString::null;
+    return QString::QString();
 }
 
 /*!

src/qtsoap.h
@@ -155,7 +155,7 @@ private:
 class QT_QTSOAP_EXPORT QtSoapQName
 {
 public:
-    QtSoapQName(const QString &name = QString::null, const QString &uri = QString::null);
+    QtSoapQName(const QString &name = QString::QString(), const QString &uri = QString::QString());
     ~QtSoapQName();
 
     QtSoapQName &operator =(const QString &s);
@@ -430,7 +430,7 @@ public:
     const QtSoapType &method() const;
     const QtSoapType &returnValue() const;
     void setMethod(const QtSoapQName &);
-    void setMethod(const QString &name, const QString &url = QString::null);
+    void setMethod(const QString &name, const QString &url = QString::QString());
     void addMethodArgument(QtSoapType *);
     void addMethodArgument(const QString &uri, const QString &name, const QString &value);
     void addMethodArgument(const QString &uri, const QString &name, bool value, int dummy);

commit e452025e4dbd08f185a9dbe2120a351dc37e8775

COMP: Fix "cannot call constructor ‘QString::QString’ directly [-fpermissive]"
This commit fixes a regression introduced in 81dc56d (removed deprecated
QString::null).

It fixes errors like the following:

  /path/to/CTK-Qt5-build/QtSOAP/src/qtsoap.h:433:79: error: cannot call constructor ‘QString::QString’ directly [-fpermissive]
    433 |     void setMethod(const QString &name, const QString &url = QString::QString());
        |

This happens with gcc 7.5.0 (at least)

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Signed-off-by: Ivaylo Dimitrov <i.dimitrov@printecgroup.com>

src/qtsoap.cpp
@@ -683,11 +683,11 @@ QtSoapType::Type QtSoapType::nameToType(const QString &name)
 }
 
 /*!
-    Returns QString::QString().
+    Returns QString().
 */
 QString QtSoapType::toString() const
 {
-    return QString::QString();
+    return QString();
 }
 
 /*!

src/qtsoap.h
@@ -155,7 +155,7 @@ private:
 class QT_QTSOAP_EXPORT QtSoapQName
 {
 public:
-    QtSoapQName(const QString &name = QString::QString(), const QString &uri = QString::QString());
+    QtSoapQName(const QString &name = QString(), const QString &uri = QString());
     ~QtSoapQName();
 
     QtSoapQName &operator =(const QString &s);
@@ -430,7 +430,7 @@ public:
     const QtSoapType &method() const;
     const QtSoapType &returnValue() const;
     void setMethod(const QtSoapQName &);
-    void setMethod(const QString &name, const QString &url = QString::QString());
+    void setMethod(const QString &name, const QString &url = QString());
     void addMethodArgument(QtSoapType *);
     void addMethodArgument(const QString &uri, const QString &name, const QString &value);
     void addMethodArgument(const QString &uri, const QString &name, bool value, int dummy);